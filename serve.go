19ab380d2c4dcca670c063ee543622caf6bd868d
Viktor <viktor@farcic.com>
2016-10-12 22:37:44 +0200

    Added Web server

--- serve.go
+++ serve.go
@@ -18,27 +18,21 @@ type Serve struct {
 }
 
 func (m *Serve) Run() error {
-	println("001")
 	if err := httpListenAndServe(":8080", m); err != nil {
-		println("003")
 		return err
 	}
-	println("003")
 	return nil
 }
 
 func (m *Serve) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	println("111")
 	httpWriterSetContentType(w, "application/json")
 	switch req.URL.Path {
 	case "/v1/docker-flow-swarm-listener/notify-services":
-		println("222")
 		services, _ := m.Service.GetServices()
 		m.Service.NotifyServicesCreate(services, 3, 5)
 		// TODO: Add response message
 		w.WriteHeader(http.StatusOK)
 	default:
-		println("333")
 		w.WriteHeader(http.StatusNotFound)
 	}
 }
